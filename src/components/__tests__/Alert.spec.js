ed0b890f25b730c995d850d0f059e02896a3b4f4
erikdeirdre <pwhite@delpwhite.org>
2024-06-30 22:10:33 -0400

    more tests

--- src/components/__tests__/Alert.spec.js
+++ src/components/__tests__/Alert.spec.js
@@ -28,21 +28,13 @@ describe('Alert', () => {
     wrapper.vm.$nextTick();
   });
 
-  test('renders alert with correct message and color', () => {
-    const snackbar = wrapper.find('snackbar[data-test="alert-snackbar"]');
-//    expect(snackbar.exists()).toBe(true);
-
-//    expect(snackbar.text()).toContain('Test message');
-
-//    expect(snackbar.attributes('color')).toBe('primary');
-  });
 
   test('closes the alert when close button is clicked', async () => {
     expect(wrapper.vm.snackbar).toBe(true);
 
-    const button = wrapper.find('[data-test="alert-btn"]');
-//    await button.trigger('click');
+    const button = wrapper.findComponent('[data-test="alert-btn"]');
+    await button.trigger('click');
 
-//    expect(wrapper.vm.snackbar).toBe(false);
+    expect(wrapper.vm.snackbar).toBe(false);
   });
 });
