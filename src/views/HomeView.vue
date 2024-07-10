2fa3526d61cb3a2a86e26c779f38d3bbb166812d
erikdeirdre <pwhite@delpwhite.org>
2024-07-09 21:32:35 -0400

    add more logging

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script setup>
+  console.log('domain');
   console.log(import.meta.env.VITE_AUTH0_DOMAIN);
+  console.log('client id');
   console.log(import.meta.env.VITE_AUTH0_CLIENT_ID);
+  console.log('scope');
   console.log(import.meta.env.VITE_AUTH0_SCOPE);
 </script>
