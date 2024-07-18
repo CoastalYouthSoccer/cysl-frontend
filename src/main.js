33580bed3c7378df9ac6a0299d7b7fb8ef5cbb9f
erikdeirdre <pwhite@delpwhite.org>
2024-07-17 21:32:53 -0400

    small tweak

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import auth0 from './auth0'
 
 import router from './router'
 
-import 'vuetify/styles' // Ensure you import Vuetify styles
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
