779aee8dca85dda501ce45d8edc313c691f8712c
erikdeirdre <pwhite@delpwhite.org>
2024-07-17 22:04:26 -0400

    remove obvious comments

--- src/components/__tests__/VenueSelect.spec.js
+++ src/components/__tests__/VenueSelect.spec.js
@@ -3,7 +3,7 @@ import { expect, it, describe, vi, beforeEach } from 'vitest'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import VenueSelect from '@/components/VenueSelect.vue'; // Adjust the import path as needed
+import VenueSelect from '@/components/VenueSelect.vue';
 import { fetchVenues } from '@/services/api.venue.js';
 
 const vuetify = createVuetify({
@@ -13,7 +13,6 @@ const vuetify = createVuetify({
 
 global.ResizeObserver = require('resize-observer-polyfill')
 
-// Mock the fetchVenues API call
 vi.mock('@/services/api.venue.js', () => ({
   fetchVenues: vi.fn(),
 }));
@@ -36,14 +35,12 @@ describe('VenueSelect', () => {
         plugins: [vuetify],
       }
     });
-    await wrapper.vm.$nextTick(); // Wait for Vue to render
+    await wrapper.vm.$nextTick();
   });
 
   it('renders v-select when venues are fetched', async () => {
-    // Check if the v-select component is rendered
     expect(wrapper.findComponent({ name: 'v-select' }).exists()).toBe(true);
 
-    // Check if the correct number of items are rendered
     const items = wrapper.findComponent({ name: 'v-select' }).props('items');
     expect(items).toHaveLength(2);
   });
@@ -51,11 +48,9 @@ describe('VenueSelect', () => {
   it('emits venueChange event when venue is selected', async () => {
     const vSelect = wrapper.findComponent({ name: 'v-select' });
 
-    // Simulate selecting a venue
     vSelect.vm.$emit('update:modelValue', { name: 'Venue 1', city: 'City 1' });
     await wrapper.vm.$nextTick();
 
-    // Check if the 'venueChange' event is emitted with the correct payload
     expect(wrapper.emitted().venueChange).toBeTruthy();
     expect(wrapper.emitted().venueChange[0]).toEqual([{ name: 'Venue 1', city: 'City 1' }]);
   });
